c428ba70aa99231a7fb083a07af7602f0c7c1b10
JeanWoinaroski <jeanwoinaroski@gmail.com>
2025-12-20 07:42:48 -0300

    fix(build): disable auto assembly info and add Properties/AssemblyInfo.cs to avoid duplicate attributes

---
+++ Users/Jwoin/ProjetoFacul/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("ProjetoFacul")]
+[assembly: AssemblyConfiguration("")] 
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0+")]
+[assembly: AssemblyProduct("ProjetoFacul")]
+[assembly: AssemblyTitle("ProjetoFacul")]
+[assembly: AssemblyVersion("1.0.0.0")]
